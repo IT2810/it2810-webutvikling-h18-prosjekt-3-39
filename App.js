5c1b649dba49b3a65cc2dc4244a1839047505481
Mikkel Sandsbråten <mikkel.sandsbraaten@gmail.com>
2018-10-08 17:24:58 +0200

    Navigering funker bra. har bare home/to donts til nå, er også lett å bygge videre på #7. ToDOnt blir lagra i async storage, henter også fra asyncstorage. Det som mangler er å gi cards med todonts litt penere design, og implementere lagring av checked state til todontsene #12

--- App.js
+++ App.js
@@ -1,27 +1,79 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {createStackNavigator, createDrawerNavigator, DrawerActions} from 'react-navigation';
 import {Provider as PaperProvider} from 'react-native-paper';
-import {createStackNavigator} from 'react-navigation';
+import {Ionicons} from '@expo/vector-icons';
+import {StyleSheet, View} from 'react-native';
+
 
 import Home from './src/components/Home';
-import Details from './src/components/Details';
+import DontDos from './src/components/DontDos';
+import NewDontDo from './src/components/NewDontDo';
 
-const RootStack = createStackNavigator(
-    {
-      Home: Home,
-      Details: Details
-    },
-    {
-      initialRouteName: 'Home'
+const styles = StyleSheet.create({
+  header: {
+    marginLeft: 15,
+    marginRight: 15
+  },
+  root: {
+    display: 'flex'
+  }
+});
+
+
+const DontDosStack = createStackNavigator({
+  DontDos: {
+    screen: DontDos,
+    navigationOptions: ({navigation}) => ({
+      title: "Don't Do",  // Title to appear in status bar
+      headerLeft: <Ionicons style={styles.header} name="md-menu" size={35}
+                            onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}/>,
+      headerRight: <Ionicons style={styles.header} name="md-add" size={35}
+                             onPress={() => navigation.navigate('NewDontDos')}/>
+    })
+  },
+  NewDontDos: {
+    screen: NewDontDo,
+    navigationOptions: {
+      title: "Ny To Don't"
     }
-);
+  }
+});
+
+const HomeStack = createStackNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: ({navigation}) => ({
+      title: 'Home',  // Title to appear in status bar
+      headerLeft: <Ionicons style={styles.header} name="md-menu" size={35}
+                            onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}/>
+    })
+  }
+});
+
+const Root = createDrawerNavigator({
+  Home: {
+    screen: HomeStack,
+    navigationOptions: {
+      title: 'Home' // Text shown in left menu
+    }
+  },
+  DontDos: {
+    screen: DontDosStack,
+    navigationOptions: {
+      title: "Don't Do"  // Text shown in left menu
+    }
+  }
+});
 
 export default class App extends React.Component {
 
   render() {
+    /*
+     * Render fuction, renders App.
+     * */
     return (
-        <PaperProvider>
-          <RootStack/>
+        <PaperProvider style={styles.root}>
+          <Root/>
         </PaperProvider>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
-import {Provider as PaperProvider, Button} from 'react-native-paper';
 import {createStackNavigator} from 'react-navigation';
+import {Button} from 'react-native-paper';
+import {Ionicons} from '@expo/vector-icons';
 
 export default class Home extends React.Component {
 
@@ -9,20 +10,10 @@ export default class Home extends React.Component {
     return (
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
           <Text>Home Screen</Text>
-          <Button mode="outlined" onPress={() => this.props.navigation.navigate('Details')}>
-            Details
+          <Button mode='outlined' onPress={() => this.props.navigation.navigate('DontDos')}>
+            Button
           </Button>
         </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignSelf: 'stretch',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
