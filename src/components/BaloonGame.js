b672000c46bd352176017ca0b5d91ac1a29f0659
fredstor <fredstor@stud.ntnu.no>
2018-10-11 12:34:29 +0200

    made some changes in BaloonGame

--- src/components/BaloonGame.js
+++ src/components/BaloonGame.js
@@ -1,25 +1,91 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Dimensions, Animated,TouchableOpacity,} from 'react-native';
 import {createStackNavigator} from 'react-navigation';
+import {Button} from "react-native-paper"
 import { black } from 'react-native-paper/src/styles/colors';
+import Canvas from "react-native-canvas"
+
+let styles = 
+{
+  button: {
+    position: "absolute",
+    height: 100,
+    width:100,
+    backgroundColor: "black",
+    left: 100,
+  }
+};
+
 
 export default class BaloonGame extends React.Component {
-  render() {
-    // TODO: Får feilmeldinger med GCCAnvas
-    return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          {/*<Text>BaloonGame</Text>
-          <GCanvas style={styles.canvas} ref="can">  </GCanvas> 
-          <canvas style={styles.canvas} ref="can"> </canvas>*/}
-            
-        </View>
-    );
+  
+  render(){
+    return(
+    <View>
+      <TopAnimation style={styles.button}>
+      </TopAnimation>
+    </View>
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  canvas:{
-    width: "100%",
-    height: "100%",
+class TopAnimation extends React.Component{
+  state = {
+    topAnim: new Animated.Value(Dimensions.get("window").height)
   }
-});
+
+  componentDidMount(){
+    Animated.timing(
+      this.state.topAnim,
+      {
+        toValue: 0,
+        duration: 10000,
+      }
+    ).start()
+  }
+ 
+  handlePress() {
+    this.state.setValue({
+      topAnim: Dimensions.get("window").height
+    });
+  }
+
+  render() {
+    let { topAnim } = this.state;
+    return(
+    <Animated.View style = {{ ...this.props.style, top: topAnim,}}>
+      {this.props.children}
+     </Animated.View>
+    );
+
+  }
+}
+
+
+
+
+
+
+    // TODO: Får feilmeldinger med GCCAnvas
+   /*
+    return (
+      <View>
+        <Canvas ref={this.handleCanvas}/>
+      </View>
+    );
+  }
+*/
+
+ /*
+  handleCanvas = (canvas) => {
+    canvas.width = Dimensions.get('window').width;
+    canvas.height = Dimensions.get('window').height;
+    ctx = canvas.getContext("2d");
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+   
+  }
+   
+}
+*/
+ 
